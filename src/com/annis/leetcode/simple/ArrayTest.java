d4e82770eb7b611c73cfe3fc6f5c30f9b3361f44
lee <annis.junjielee@gmail.com>
2021-04-18 11:39:59 +0800

    加一

--- src/com/annis/leetcode/simple/ArrayTest.java
+++ src/com/annis/leetcode/simple/ArrayTest.java
@@ -7,12 +7,63 @@ public class ArrayTest {
         //顺序数组去重
 //        removeDuplicatesTest();
         //买卖股票的最佳时机
-        maxProfitTest();
+        //maxProfitTest();
+
+
+        //加一
+        plusOneTest();
+    }
+
+    private static void plusOneTest() {
+        int[] preces = new int[]{0};
+        int[] ints = plusOne(preces);
+        System.out.println(ArrayUtil.toString(ints));
+    }
+
+
+    /**
+     * 加一
+     * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+     * <p>
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     * <p>
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     * <p>
+     * 输入：digits = [1,2,3]
+     * 输出：[1,2,4]
+     * 解释：输入数组表示数字 123。
+     */
+    /*
+    执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：36.9 MB, 在所有 Java 提交中击败了58.70%的用户
+     */
+    public static int[] plusOne(int[] digits) {
+        boolean add = true;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (add) {
+                if (digits[i] == 9) {
+                    digits[i] = 0;
+                } else {
+                    digits[i] += 1;
+                    add = false;
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        if (add) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            System.arraycopy(digits, 0, result, 1, digits.length);
+            return result;
+        }
+        return digits;
     }
 
     private static void maxProfitTest() {
 //        int[] preces = new int[]{7, 1, 5, 3, 6, 4};
-        int[] preces = new int[]{1,2,3,4,5};
+        int[] preces = new int[]{1, 2, 3, 4, 5};
         int count = maxProfit(preces);
         System.out.println("共获利:" + count);
     }
